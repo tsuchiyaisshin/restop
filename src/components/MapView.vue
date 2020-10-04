d77dfe8c30c2f3b7fa3f4fc6eb81a2441e1ba16a
土屋一心 <tsuchiya.isshin@win-techno.com>
2020-10-04 15:55:45 +0900

    map vue

---
+++ src/components/MapView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <GmapMap
+                :center="{lat:10, lng:10}"
+                :zoom="7"
+                map-type-id="terrain"
+                style="width: 500px; height: 300px"
+        >
+            <GmapMarker
+                    :key="index"
+                    v-for="(m, index) in markers"
+                    :position="m.position"
+                    :clickable="true"
+                    :draggable="true"
+                    @click="center=m.position"
+            />
+        </GmapMap>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Map',
+    data() {
+        return {
+            markers: [{ position: { lat: 10, lng: 10 } }]
+        }
+    },
+}
+
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAP_API,
+    libraries: 'places',
+    region: 'JP',
+    language: 'ja'
+  }
+})
 
 Vue.config.productionTip = false
 
