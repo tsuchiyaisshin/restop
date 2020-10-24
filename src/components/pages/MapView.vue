46c942b2bcaa4bc2674299a97224540bc1a1447f
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-10-24 20:23:02 +0900

    change markers

--- src/components/pages/MapView.vue
+++ src/components/pages/MapView.vue
@@ -21,7 +21,7 @@
       :position="m.position"
       :clickable="true"
       :draggable="false"
-      :icon="{ url: require('../../assets/markers/food.svg')}"
+      :icon="{ url: require('../../assets/markers/' + m.icon)}"
       @click="center=m.position"
     />
   </GmapMap>
@@ -43,68 +43,86 @@
             position: {
               lat: 35.617325,
               lng: 139.564624
-            }
+            },
+            icon: 'current.svg'
           },
           {
             title: 'ガスト',
             position: {
               lat: 35.616463,
               lng: 139.544759
-            }
+            },
+            icon: 'food.svg'
           },
           {
+            title: 'ドトール',
             position: {
               lat: 35.615218,
               lng: 139.542351
-            }
+            },
+            icon: 'food.svg'
           },
           {
+            title: 'カフェ',
             position: {
               lat: 35.620048,
               lng: 139.561672
-            }
+            },
+            icon: 'food.svg'
           },
           {
+            title: 'ジョナサン',
             position: {
               lat: 35.612794,
               lng: 139.539650
-            }
+            },
+            icon: 'food.svg'
           },
           {
             position: {
               lat: 35.622104,
               lng: 139.550595
-            }
+            },
+            icon: 'food.svg'
           },
           {
+            title: 'マクドナルド',
             position: {
               lat: 35.621952,
               lng: 139.549783
-            }
+            },
+            icon: 'food.svg'
           },
           {
+            title: 'モコモコシップ',
             position: {
               lat: 35.616292,
               lng: 139.564151
-            }
+            },
+            icon: 'karaoke.svg'
           },
           {
+            title: 'ケンタッキー',
             position: {
               lat: 35.616577,
               lng: 139.564022
-            }
+            },
+            icon: 'food.svg'
           },
           {
+            title: 'モコモコ',
             position: {
               lat: 35.618014,
               lng: 139.566869
-            }
+            },
+            icon: 'karaoke.svg'
           },
           {
             position: {
               lat: 35.618705,
               lng: 139.562450
-            }
+            },
+            icon: 'food.svg'
           }
           ],
         center: {lat: 35.617325, lng: 139.564624},
