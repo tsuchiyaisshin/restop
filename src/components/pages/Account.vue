6165983652e24f0e8160ed8f19fd82a291ea1714
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-11-05 18:37:00 +0900

    edit account pages

--- src/components/pages/Account.vue
+++ src/components/pages/Account.vue
@@ -1,117 +1,102 @@
 <template>
   <v-container>
-    <v-row no-gutters>
-      <v-col cols="12" md="6">
-        <v-card ref="form" class="ma-3">
-          <v-card-text>
-            <v-text-field
-              ref="name"
-              v-model="name"
-              :rules="[() => !!name || 'This field is required']"
-              :error-messages="errorMessages"
-              label="Full Name"
-              placeholder="Isshin Tsuchiya"
-              required
-            ></v-text-field>
-            <v-text-field
-              ref="address"
-              v-model="address"
-              :rules="[
+    <v-row align="center" justify="center" no-gutters>
+      <div class="display-1 mr-3">
+        Welcome! Senshu Taro!
+      </div>
+      <v-avatar size="100">
+        <v-img
+          lazy-src="https://picsum.photos/id/11/10/6"
+          src="https://picsum.photos/id/11/500/300"
+        >
+        </v-img>
+      </v-avatar>
+    </v-row>
+    <v-card ref="form" class="ma-3" width="400">
+      <v-card-text>
+        <v-row align="center" no-gutters>
+          <div>
+            Avatar Image
+          </div>
+          <v-spacer></v-spacer>
+          <v-avatar size="100">
+            <v-hover v-slot="{ hover }">
+              <v-img
+                lazy-src="https://picsum.photos/id/11/10/6"
+                src="https://picsum.photos/id/11/500/300"
+              >
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-fast-in-fast-out light-green darken-2 v-card--reveal display-1 white--text"
+                    style="height: 100%; width: 100%; padding-left: 20px; padding-top: 30px"
+                    @click="editImage"
+                  >
+                    Edit
+                  </div>
+                </v-expand-transition>
+              </v-img>
+            </v-hover>
+          </v-avatar>
+        </v-row>
+        <v-text-field
+          ref="name"
+          v-model="name"
+          :rules="[() => !!name || 'This field is required']"
+          :error-messages="errorMessages"
+          label="Full Name"
+          placeholder="Senshu Taro"
+          required
+        ></v-text-field>
+        <v-text-field
+          ref="address"
+          v-model="address"
+          :rules="[
                 () => !!address || 'This field is required',
                 () =>
                   (!!address && address.length <= 25) ||
-                  'Address must be less than 25 characters',
+                  'Email must be less than 25 characters',
                 addressCheck,
               ]"
-              label="Address Line"
-              placeholder="Ikuta"
-              counter="25"
-              required
-            ></v-text-field>
-            <v-text-field
-              ref="city"
-              v-model="city"
-              :rules="[() => !!city || 'This field is required', addressCheck]"
-              label="City"
-              placeholder="Kawasaki"
-              required
-            ></v-text-field>
-            <v-text-field
-              ref="state"
-              v-model="state"
-              :rules="[() => !!state || 'This field is required']"
-              label="State/Province/Region"
-              required
-              placeholder="Kanagawa"
-            ></v-text-field>
-            <v-text-field
-              ref="zip"
-              v-model="zip"
-              :rules="[() => !!zip || 'This field is required']"
-              label="ZIP / Postal Code"
-              required
-              placeholder="000-0000"
-            ></v-text-field>
-            <v-autocomplete
-              ref="country"
-              v-model="country"
-              :rules="[() => !!country || 'This field is required']"
-              :items="countries"
-              label="Country"
-              placeholder="Select..."
-              required
-            ></v-autocomplete>
-          </v-card-text>
-          <v-divider class="mt-12"></v-divider>
-          <v-card-actions>
-            <v-btn text>
-              Cancel
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-slide-x-reverse-transition>
-              <v-tooltip v-if="formHasErrors" left>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    icon
-                    class="my-0"
-                    v-bind="attrs"
-                    @click="resetForm"
-                    v-on="on"
-                  >
-                    <v-icon>mdi-refresh</v-icon>
-                  </v-btn>
-                </template>
-                <span>Refresh form</span>
-              </v-tooltip>
-            </v-slide-x-reverse-transition>
-            <v-btn color="primary" text @click="submit">
-              Submit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-avatar size="200">
-          <v-hover v-slot="{ hover }">
-            <v-img
-              lazy-src="https://picsum.photos/id/11/10/6"
-              src="https://picsum.photos/id/11/500/300"
-            >
-              <v-expand-transition>
-                <div
-                  v-if="hover"
-                  class="d-flex transition-fast-in-fast-out light-green darken-2 v-card--reveal display-3 white--text"
-                  style="height: 100%; width: 100%; padding-left: 50px; padding-top: 70px"
-                  @click="editImage"
-                >
-                  Edit
-                </div>
-              </v-expand-transition>
-            </v-img>
-          </v-hover>
-        </v-avatar>
-      </v-col>
-    </v-row>
+          label="Email"
+          placeholder="example@example.com"
+          counter="25"
+          required
+        ></v-text-field>
+        <v-autocomplete
+          ref="country"
+          v-model="country"
+          :rules="[() => !!country || 'This field is required']"
+          :items="countries"
+          label="Country"
+          placeholder="Select..."
+          required
+        ></v-autocomplete>
+      </v-card-text>
+      <v-divider class="mt-12"></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-slide-x-reverse-transition>
+          <v-tooltip v-if="formHasErrors" left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                class="my-0"
+                v-bind="attrs"
+                @click="resetForm"
+                v-on="on"
+              >
+                <v-icon>mdi-refresh</v-icon>
+              </v-btn>
+            </template>
+            <span>Refresh form</span>
+          </v-tooltip>
+        </v-slide-x-reverse-transition>
+        <v-btn color="primary" text @click="submit">
+          Submit
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
