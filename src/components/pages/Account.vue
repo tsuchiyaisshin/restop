30286aa4156fe3510b37bdeb6702d818a83e3b48
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-11-06 18:57:18 +0900

    fix accout page

--- src/components/pages/Account.vue
+++ src/components/pages/Account.vue
@@ -12,91 +12,154 @@
         </v-img>
       </v-avatar>
     </v-row>
-    <v-card ref="form" class="ma-3" width="400">
-      <v-card-text>
-        <v-row align="center" no-gutters>
-          <div>
-            Avatar Image
+    <v-row class="mt-10">
+      <v-col cols="12" md="6">
+        <v-card height="200">
+          <div class="d-flex">
+            <v-card-title>
+              Your Profile
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn class="ma-4" color="light-green" dark @click="openDialog"
+              >edit</v-btn
+            >
           </div>
+          <div class="d-flex text-left ml-4 mt-4">
+            <v-list-item-title class="font-weight-medium">
+              Full Name:
+            </v-list-item-title>
+            <v-list-item-title class="text-right mr-4">
+              Senshu Taro
+            </v-list-item-title>
+          </div>
+          <div class="ml-4 mr-4">
+            <v-divider></v-divider>
+          </div>
+          <div class="d-flex text-left ml-4 mt-4">
+            <v-list-item-title class="font-weight-medium">
+              Email:
+            </v-list-item-title>
+            <v-list-item-title class="text-right mr-4">
+              example@example.com
+            </v-list-item-title>
+          </div>
+          <div class="ml-4 mr-4">
+            <v-divider></v-divider>
+          </div>
+          <div class="d-flex text-left ml-4 mt-4">
+            <v-list-item-title class="font-weight-medium">
+              Country:
+            </v-list-item-title>
+            <v-list-item-title class="text-right mr-4">
+              Japan
+            </v-list-item-title>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card height="200">
+          <v-card-title>
+            Add Position
+          </v-card-title>
+          <v-card-text>
+            sorry.. I develop now..
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-dialog
+      v-model="dialog"
+      width="400"
+    >
+      <v-card ref="form" class="pa-4">
+        <v-card-text>
+          <v-row align="center" no-gutters>
+            <div>
+              Avatar Image
+            </div>
+            <v-spacer></v-spacer>
+            <v-avatar size="100">
+              <v-hover v-slot="{ hover }">
+                <v-img
+                  lazy-src="https://picsum.photos/id/11/10/6"
+                  src="https://picsum.photos/id/11/500/300"
+                >
+                  <v-expand-transition>
+                    <div
+                      v-if="hover"
+                      class="d-flex transition-fast-in-fast-out light-green darken-2 v-card--reveal display-1 white--text"
+                      style="height: 100%; width: 100%; padding-left: 20px; padding-top: 30px"
+                      @click="editImage"
+                    >
+                      Edit
+                    </div>
+                  </v-expand-transition>
+                </v-img>
+              </v-hover>
+            </v-avatar>
+          </v-row>
+          <v-text-field
+            ref="name"
+            v-model="name"
+            :rules="[() => !!name || 'This field is required']"
+            :error-messages="errorMessages"
+            label="Full Name"
+            placeholder="Senshu Taro"
+            required
+          ></v-text-field>
+          <v-text-field
+            ref="address"
+            v-model="address"
+            :rules="[
+            () => !!address || 'This field is required',
+            () =>
+              (!!address && address.length <= 25) ||
+              'Email must be less than 25 characters',
+            addressCheck,
+          ]"
+            label="Email"
+            placeholder="example@example.com"
+            counter="25"
+            required
+          ></v-text-field>
+          <v-autocomplete
+            ref="country"
+            v-model="country"
+            :rules="[() => !!country || 'This field is required']"
+            :items="countries"
+            label="Country"
+            placeholder="Select..."
+            required
+          ></v-autocomplete>
+        </v-card-text>
+        <v-divider class="mt-12"></v-divider>
+        <v-card-actions>
+          <v-btn text @click="openDialog">
+            close
+          </v-btn>
           <v-spacer></v-spacer>
-          <v-avatar size="100">
-            <v-hover v-slot="{ hover }">
-              <v-img
-                lazy-src="https://picsum.photos/id/11/10/6"
-                src="https://picsum.photos/id/11/500/300"
-              >
-                <v-expand-transition>
-                  <div
-                    v-if="hover"
-                    class="d-flex transition-fast-in-fast-out light-green darken-2 v-card--reveal display-1 white--text"
-                    style="height: 100%; width: 100%; padding-left: 20px; padding-top: 30px"
-                    @click="editImage"
-                  >
-                    Edit
-                  </div>
-                </v-expand-transition>
-              </v-img>
-            </v-hover>
-          </v-avatar>
-        </v-row>
-        <v-text-field
-          ref="name"
-          v-model="name"
-          :rules="[() => !!name || 'This field is required']"
-          :error-messages="errorMessages"
-          label="Full Name"
-          placeholder="Senshu Taro"
-          required
-        ></v-text-field>
-        <v-text-field
-          ref="address"
-          v-model="address"
-          :rules="[
-                () => !!address || 'This field is required',
-                () =>
-                  (!!address && address.length <= 25) ||
-                  'Email must be less than 25 characters',
-                addressCheck,
-              ]"
-          label="Email"
-          placeholder="example@example.com"
-          counter="25"
-          required
-        ></v-text-field>
-        <v-autocomplete
-          ref="country"
-          v-model="country"
-          :rules="[() => !!country || 'This field is required']"
-          :items="countries"
-          label="Country"
-          placeholder="Select..."
-          required
-        ></v-autocomplete>
-      </v-card-text>
-      <v-divider class="mt-12"></v-divider>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-slide-x-reverse-transition>
-          <v-tooltip v-if="formHasErrors" left>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                class="my-0"
-                v-bind="attrs"
-                @click="resetForm"
-                v-on="on"
-              >
-                <v-icon>mdi-refresh</v-icon>
-              </v-btn>
-            </template>
-            <span>Refresh form</span>
-          </v-tooltip>
-        </v-slide-x-reverse-transition>
-        <v-btn color="primary" text @click="submit">
-          Submit
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+          <v-slide-x-reverse-transition>
+            <v-tooltip v-if="formHasErrors" left>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  class="my-0"
+                  v-bind="attrs"
+                  @click="resetForm"
+                  v-on="on"
+                >
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+              </template>
+              <span>Refresh form</span>
+            </v-tooltip>
+          </v-slide-x-reverse-transition>
+          <v-btn color="primary" text @click="submit">
+            Submit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -320,6 +383,7 @@ export default {
     zip: null,
     country: null,
     formHasErrors: false,
+    dialog: false,
   }),
 
   computed: {
@@ -366,6 +430,13 @@ export default {
     },
     editImage() {
       console.log('click')
+    },
+    openDialog() {
+      if (this.dialog) {
+        this.dialog = false
+      } else {
+        this.dialog = true
+      }
     },
   },
 }
