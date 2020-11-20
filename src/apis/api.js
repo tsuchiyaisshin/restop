eed43d8ee014e2405de09ff24793c5928e6780d6
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-11-20 23:45:03 +0900

    API連携

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,14 +2,13 @@ import service from '../utils/service'
 
 export const api = {
   async getRoute(origin) {
-    await service
+    return await service
       .post('http://localhost:8080/getRoute', origin, {
         withCredentials: true,
       })
       .then(result => {
-        window.console.log(result)
+        return result.data
       })
-      .catch(result => {
-      })
+      .catch(result => {})
   },
 }
---
+++ src/components/atoms/DirectionsRenderer.js
@@ -0,0 +1,40 @@
+import { MapElementFactory } from 'vue2-google-maps'
+
+export default MapElementFactory({
+  name: 'directionsRenderer',
+  ctr() {
+    return google.maps.DirectionRenderer
+  },
+
+  events: [],
+
+  props: {
+    origin: { type: Object },
+    destination: { type: Object },
+    travelMode: { type: String }
+  },
+
+  afterCreate(directionsRenderer) {
+    let directionsService = new google.maps.DirectionsService();
+
+    this.$watch(
+      () => [this.origin, this.destination, this.travelMode],
+      () => {
+        let { origin, destination, travelMode } = this;
+        if (!origin || !destination || !travelMode) return;
+
+        directionsService.route(
+          {
+            origin,
+            destination,
+            travelMode
+          },
+          (response, status) => {
+            if (status !== "OK") return;
+            directionsRenderer.setDirections(response);
+          }
+        );
+      }
+    );
+  }
+})
