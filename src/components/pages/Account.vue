ac7140ec62a834f7b67545582b25b346f721e6c6
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-12-02 14:43:46 +0900

    Fix Account and History

--- src/components/pages/Account.vue
+++ src/components/pages/Account.vue
@@ -14,7 +14,7 @@
     </v-row>
     <v-row class="mt-10">
       <v-col cols="12" md="6">
-        <v-card height="200">
+        <v-card height="150">
           <div class="d-flex">
             <v-card-title>
               Your Profile
@@ -34,25 +34,6 @@
           </div>
           <div class="ml-4 mr-4">
             <v-divider></v-divider>
-          </div>
-          <div class="d-flex text-left ml-4 mt-4">
-            <v-list-item-title class="font-weight-medium">
-              Email:
-            </v-list-item-title>
-            <v-list-item-title class="text-right mr-4">
-              example@example.com
-            </v-list-item-title>
-          </div>
-          <div class="ml-4 mr-4">
-            <v-divider></v-divider>
-          </div>
-          <div class="d-flex text-left ml-4 mt-4">
-            <v-list-item-title class="font-weight-medium">
-              Country:
-            </v-list-item-title>
-            <v-list-item-title class="text-right mr-4">
-              Japan
-            </v-list-item-title>
           </div>
         </v-card>
       </v-col>
@@ -104,30 +85,6 @@
             placeholder="Senshu Taro"
             required
           ></v-text-field>
-          <v-text-field
-            ref="address"
-            v-model="address"
-            :rules="[
-              () => !!address || 'This field is required',
-              () =>
-                (!!address && address.length <= 25) ||
-                'Email must be less than 25 characters',
-              addressCheck,
-            ]"
-            label="Email"
-            placeholder="example@example.com"
-            counter="25"
-            required
-          ></v-text-field>
-          <v-autocomplete
-            ref="country"
-            v-model="country"
-            :rules="[() => !!country || 'This field is required']"
-            :items="countries"
-            label="Country"
-            placeholder="Select..."
-            required
-          ></v-autocomplete>
         </v-card-text>
         <v-divider class="mt-12"></v-divider>
         <v-card-actions>
@@ -164,214 +121,6 @@
 export default {
   name: 'Account',
   data: () => ({
-    countries: [
-      'Afghanistan',
-      'Albania',
-      'Algeria',
-      'Andorra',
-      'Angola',
-      'Anguilla',
-      'Antigua &amp; Barbuda',
-      'Argentina',
-      'Armenia',
-      'Aruba',
-      'Australia',
-      'Austria',
-      'Azerbaijan',
-      'Bahamas',
-      'Bahrain',
-      'Bangladesh',
-      'Barbados',
-      'Belarus',
-      'Belgium',
-      'Belize',
-      'Benin',
-      'Bermuda',
-      'Bhutan',
-      'Bolivia',
-      'Bosnia &amp; Herzegovina',
-      'Botswana',
-      'Brazil',
-      'British Virgin Islands',
-      'Brunei',
-      'Bulgaria',
-      'Burkina Faso',
-      'Burundi',
-      'Cambodia',
-      'Cameroon',
-      'Cape Verde',
-      'Cayman Islands',
-      'Chad',
-      'Chile',
-      'China',
-      'Colombia',
-      'Congo',
-      'Cook Islands',
-      'Costa Rica',
-      'Cote D Ivoire',
-      'Croatia',
-      'Cruise Ship',
-      'Cuba',
-      'Cyprus',
-      'Czech Republic',
-      'Denmark',
-      'Djibouti',
-      'Dominica',
-      'Dominican Republic',
-      'Ecuador',
-      'Egypt',
-      'El Salvador',
-      'Equatorial Guinea',
-      'Estonia',
-      'Ethiopia',
-      'Falkland Islands',
-      'Faroe Islands',
-      'Fiji',
-      'Finland',
-      'France',
-      'French Polynesia',
-      'French West Indies',
-      'Gabon',
-      'Gambia',
-      'Georgia',
-      'Germany',
-      'Ghana',
-      'Gibraltar',
-      'Greece',
-      'Greenland',
-      'Grenada',
-      'Guam',
-      'Guatemala',
-      'Guernsey',
-      'Guinea',
-      'Guinea Bissau',
-      'Guyana',
-      'Haiti',
-      'Honduras',
-      'Hong Kong',
-      'Hungary',
-      'Iceland',
-      'India',
-      'Indonesia',
-      'Iran',
-      'Iraq',
-      'Ireland',
-      'Isle of Man',
-      'Israel',
-      'Italy',
-      'Jamaica',
-      'Japan',
-      'Jersey',
-      'Jordan',
-      'Kazakhstan',
-      'Kenya',
-      'Kuwait',
-      'Kyrgyz Republic',
-      'Laos',
-      'Latvia',
-      'Lebanon',
-      'Lesotho',
-      'Liberia',
-      'Libya',
-      'Liechtenstein',
-      'Lithuania',
-      'Luxembourg',
-      'Macau',
-      'Macedonia',
-      'Madagascar',
-      'Malawi',
-      'Malaysia',
-      'Maldives',
-      'Mali',
-      'Malta',
-      'Mauritania',
-      'Mauritius',
-      'Mexico',
-      'Moldova',
-      'Monaco',
-      'Mongolia',
-      'Montenegro',
-      'Montserrat',
-      'Morocco',
-      'Mozambique',
-      'Namibia',
-      'Nepal',
-      'Netherlands',
-      'Netherlands Antilles',
-      'New Caledonia',
-      'New Zealand',
-      'Nicaragua',
-      'Niger',
-      'Nigeria',
-      'Norway',
-      'Oman',
-      'Pakistan',
-      'Palestine',
-      'Panama',
-      'Papua New Guinea',
-      'Paraguay',
-      'Peru',
-      'Philippines',
-      'Poland',
-      'Portugal',
-      'Puerto Rico',
-      'Qatar',
-      'Reunion',
-      'Romania',
-      'Russia',
-      'Rwanda',
-      'Saint Pierre &amp; Miquelon',
-      'Samoa',
-      'San Marino',
-      'Satellite',
-      'Saudi Arabia',
-      'Senegal',
-      'Serbia',
-      'Seychelles',
-      'Sierra Leone',
-      'Singapore',
-      'Slovakia',
-      'Slovenia',
-      'South Africa',
-      'South Korea',
-      'Spain',
-      'Sri Lanka',
-      'St Kitts &amp; Nevis',
-      'St Lucia',
-      'St Vincent',
-      'St. Lucia',
-      'Sudan',
-      'Suriname',
-      'Swaziland',
-      'Sweden',
-      'Switzerland',
-      'Syria',
-      'Taiwan',
-      'Tajikistan',
-      'Tanzania',
-      'Thailand',
-      `Timor L'Este`,
-      'Togo',
-      'Tonga',
-      'Trinidad &amp; Tobago',
-      'Tunisia',
-      'Turkey',
-      'Turkmenistan',
-      'Turks &amp; Caicos',
-      'Uganda',
-      'Ukraine',
-      'United Arab Emirates',
-      'United Kingdom',
-      'United States',
-      'Uruguay',
-      'Uzbekistan',
-      'Venezuela',
-      'Vietnam',
-      'Virgin Islands (US)',
-      'Yemen',
-      'Zambia',
-      'Zimbabwe',
-    ],
     errorMessages: '',
     name: null,
     address: null,
--- src/components/pages/History.vue
+++ src/components/pages/History.vue
@@ -2,7 +2,7 @@
   <div>
     <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="items"
       item-key="name"
       class="elevation-1"
       :search="search"
@@ -20,7 +20,7 @@
           <td></td>
           <td>
             <v-text-field
-              v-model="calories"
+              v-model="asses"
               type="number"
               label="Less than"
             ></v-text-field>
@@ -37,87 +37,27 @@ export default {
   name: 'History',
   data: () => ({
     search: '',
-    calories: '',
-    desserts: [
+    asses: '',
+    items: [
       {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: '1%',
+        name: 'ガスト',
+        time: '11/30',
+        asses: 10.0,
       },
       {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: '1%',
+        name: 'マクド',
+        time: '11/30',
+        asses: 6.0,
       },
       {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: '7%',
+        name: 'カーシェア',
+        time: '11/30',
+        asses: 8.0,
       },
       {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: '8%',
-      },
-      {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: '16%',
-      },
-      {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: '0%',
-      },
-      {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: '2%',
-      },
-      {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: '45%',
-      },
-      {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: '22%',
-      },
-      {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: '6%',
+        name: 'カフェ',
+        time: '11/30',
+        asses: 2.0,
       },
     ],
   }),
@@ -125,24 +65,24 @@ export default {
     headers() {
       return [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Title',
           align: 'start',
           sortable: false,
           value: 'name',
         },
         {
-          text: 'Calories',
-          value: 'calories',
+          text: 'Time',
+          value: 'time',
+        },
+        {
+          text: 'Assessment',
+          value: 'asses',
           filter: value => {
-            if (!this.calories) return true
+            if (!this.asses) return true
 
-            return value < parseInt(this.calories)
+            return value < parseInt(this.asses)
           },
         },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' },
       ]
     },
   },
