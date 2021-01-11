1cadf22f30ece3a5340cb91c1a3823b1a030f1cd
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2021-01-11 20:16:42 +0900

    feat:　スポット追加機能の画面実装

--- src/components/pages/Account.vue
+++ src/components/pages/Account.vue
@@ -12,7 +12,7 @@
     </v-row>
     <v-row class="mt-10">
       <v-col cols="12" md="6">
-        <v-card height="150">
+        <v-card class="pb-5">
           <div class="d-flex">
             <v-card-title>
               Your Profile
@@ -36,12 +36,111 @@
         </v-card>
       </v-col>
       <v-col cols="12" md="6">
-        <v-card height="200">
+        <v-card class="pb-5">
           <v-card-title>
             Add Position
           </v-card-title>
           <v-card-text>
             sorry.. I develop now..
+          </v-card-text>
+          <v-card-text>
+            <div class="d-flex">
+              <div class="mt-1 pt-3 font-weight-medium">
+                Position name
+              </div>
+              <v-spacer></v-spacer>
+              <v-text-field
+                v-model="position"
+                placeholder="ガスト生田駅前店"
+              >
+              </v-text-field>
+            </div>
+            <div class="d-flex">
+              <div class="mt-1 pt-3 font-weight-medium">
+                Time
+              </div>
+              <v-spacer></v-spacer>
+              <v-menu
+                ref="menu"
+                v-model="menu2"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="startTime"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="startTime"
+                    style="width: 25px"
+                    label="Start Time"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  ampm-in-title
+                  v-if="menu2"
+                  v-model="startTime"
+                  full-width
+                  @click:minute="$refs.menu.save(startTime)"
+                ></v-time-picker>
+              </v-menu>
+              <div class="mt-1 pt-3 font-weight-medium pl-5 pr-5 pt-4">
+                ~
+              </div>
+              <v-menu
+                ref="menu4"
+                v-model="menu3"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="endTime"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="endTime"
+                    style="width: 25px"
+                    label="End Time"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  ampm-in-title
+                  v-if="menu3"
+                  v-model="endTime"
+                  full-width
+                  @click:minute="$refs.menu4.save(endTime)"
+                ></v-time-picker>
+              </v-menu>
+            </div>
+            <div class="d-flex">
+              <div class="mt-1 pt-3 font-weight-medium">
+                Icon
+              </div>
+              <v-spacer></v-spacer>
+              <v-select
+                class="ml-10"
+                v-model="markerIcon"
+                :items="markerIcons"
+                menu-props="auto"
+                label="Select"
+                hide-details
+                single-line
+              >
+                <template v-slot:prepend>
+                  <img v-model="iconSvg" width="25px" :src="iconSvg">
+                </template>
+              </v-select>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -125,6 +224,25 @@ export default {
     name: null,
     formHasErrors: false,
     dialog: false,
+    position: '',
+    startTime: '',
+    endTime: '',
+    markerIcon: 'Food',
+    menu2: false,
+    menu3: false,
+    iconSvg: require('../../assets/markers/food.svg'),
+    markerIcons: [
+      'Food',
+      'CoWorking Space',
+      'Car Rental',
+      'Karaoke'
+    ],
+    icons: new Map([
+      [ 'Food', 'food.svg'],
+      [ 'CoWorking Space', 'co-working-space.svg'],
+      [ 'Car Rental', 'car.svg'],
+      [ 'Karaoke', 'karaoke.svg']
+    ])
   }),
 
   computed: {
@@ -139,9 +257,15 @@ export default {
     name() {
       this.errorMessages = ''
     },
+    markerIcon(val) {
+      this.iconSvg = require('../../assets/markers/' + this.icons.get(val))
+    }
   },
 
   methods: {
+    log() {
+      console.log(this.markerIcon)
+    },
     addressCheck() {
       this.errorMessages = this.address && !this.name ? `Hey! I'm required` : ''
 
