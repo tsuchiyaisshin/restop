cf5d32a0ffaec2eddeb99064d4e8919c2b3cf511
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2021-01-13 13:47:15 +0900

    refactor: componentの細分化

---
+++ src/components/atoms/EditNameCard.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <v-card class="pb-5">
+      <div class="d-flex">
+        <v-card-title>
+          Your Profile
+        </v-card-title>
+        <v-spacer></v-spacer>
+        <v-btn class="ma-4" color="light-green" dark @click="openDialog">
+          edit
+        </v-btn>
+      </div>
+      <div class="d-flex text-left ml-4 mt-4">
+        <v-list-item-title class="font-weight-medium">
+          Full Name:
+        </v-list-item-title>
+        <v-list-item-title class="text-right mr-4">
+          {{ $store.state.username }}
+        </v-list-item-title>
+      </div>
+      <div class="ml-4 mr-4">
+        <v-divider></v-divider>
+      </div>
+    </v-card>
+    <v-dialog v-model="dialog" width="400">
+      <v-card ref="form" class="pa-4">
+        <v-card-text>
+          <v-row align="center" no-gutters>
+            <div>
+              Avatar Image
+            </div>
+            <v-spacer></v-spacer>
+            <v-avatar size="100">
+              <v-hover v-slot="{ hover }">
+                <v-img
+                  lazy-src="https://picsum.photos/id/11/10/6"
+                  src="https://picsum.photos/id/11/500/300"
+                >
+                  <v-expand-transition>
+                    <div
+                      v-if="hover"
+                      class="d-flex transition-fast-in-fast-out light-green darken-2 v-card--reveal display-1 white--text"
+                      style="height: 100%; width: 100%; padding-left: 20px; padding-top: 30px"
+                      @click="editImage"
+                    >
+                      Edit
+                    </div>
+                  </v-expand-transition>
+                </v-img>
+              </v-hover>
+            </v-avatar>
+          </v-row>
+          <v-text-field
+            ref="name"
+            v-model="name"
+            :rules="[() => !!name || 'This field is required']"
+            :error-messages="errorMessages"
+            label="Full Name"
+            :placeholder="$store.state.username"
+            required
+          ></v-text-field>
+        </v-card-text>
+        <v-divider class="mt-12"></v-divider>
+        <v-card-actions>
+          <v-btn text @click="openDialog">
+            close
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-slide-x-reverse-transition>
+            <v-tooltip v-if="formHasErrors" left>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  class="my-0"
+                  v-bind="attrs"
+                  @click="resetForm"
+                  v-on="on"
+                >
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+              </template>
+              <span>Refresh form</span>
+            </v-tooltip>
+          </v-slide-x-reverse-transition>
+          <v-btn color="primary" text @click="submit">
+            Submit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import {api} from "../../apis/api";
+
+export default {
+  name: 'EditNameCard',
+  data: () => ({
+    name: null,
+    dialog: false,
+    errorMessages: '',
+    formHasErrors: false,
+  }),
+
+  computed: {
+    form() {
+      return {
+        name: this.name,
+      }
+    },
+  },
+
+  watch: {
+    name() {
+      this.errorMessages = ''
+    }
+  },
+
+  methods: {
+    //TODO: formのvalidationを後々修正する。
+    addressCheck() {
+      this.errorMessages = this.address && !this.name ? `Hey! I'm required` : ''
+
+      return true
+    },
+    resetForm() {
+      this.errorMessages = []
+      this.formHasErrors = false
+
+      Object.keys(this.form).forEach(f => {
+        this.$refs[f].reset()
+      })
+    },
+    submit() {
+      this.formHasErrors = false
+      const body = {
+        userId: this.$store.state.user.username,
+        name: this.name,
+      }
+      Object.keys(this.form).forEach(f => {
+        if (!this.form[f]) this.formHasErrors = true
+
+        this.$refs[f].validate(true)
+
+        if (!this.formHasErrors) {
+          this.$store.commit('setUsername', this.name)
+          api.saveUser(body)
+        }
+      })
+      this.dialog = false
+    },
+    editImage() {
+      console.log('click')
+    },
+    openDialog() {
+      if (this.dialog) {
+        this.dialog = false
+      } else {
+        this.dialog = true
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- src/components/pages/Account.vue
+++ src/components/pages/Account.vue
@@ -12,28 +12,7 @@
     </v-row>
     <v-row class="mt-10">
       <v-col cols="12" md="6">
-        <v-card class="pb-5">
-          <div class="d-flex">
-            <v-card-title>
-              Your Profile
-            </v-card-title>
-            <v-spacer></v-spacer>
-            <v-btn class="ma-4" color="light-green" dark @click="openDialog">
-              edit
-            </v-btn>
-          </div>
-          <div class="d-flex text-left ml-4 mt-4">
-            <v-list-item-title class="font-weight-medium">
-              Full Name:
-            </v-list-item-title>
-            <v-list-item-title class="text-right mr-4">
-              {{ $store.state.username }}
-            </v-list-item-title>
-          </div>
-          <div class="ml-4 mr-4">
-            <v-divider></v-divider>
-          </div>
-        </v-card>
+        <edit-name-card></edit-name-card>
       </v-col>
       <v-col cols="12" md="6">
         <v-card class="pb-5">
@@ -49,10 +28,7 @@
                 Position name
               </div>
               <v-spacer></v-spacer>
-              <v-text-field
-                v-model="position"
-                placeholder="ガスト生田駅前店"
-              >
+              <v-text-field v-model="position" placeholder="ガスト生田駅前店">
               </v-text-field>
             </div>
             <div class="d-flex">
@@ -82,9 +58,9 @@
                   ></v-text-field>
                 </template>
                 <v-time-picker
-                  ampm-in-title
                   v-if="menu2"
                   v-model="startTime"
+                  ampm-in-title
                   full-width
                   @click:minute="$refs.menu.save(startTime)"
                 ></v-time-picker>
@@ -114,9 +90,9 @@
                   ></v-text-field>
                 </template>
                 <v-time-picker
-                  ampm-in-title
                   v-if="menu3"
                   v-model="endTime"
+                  ampm-in-title
                   full-width
                   @click:minute="$refs.menu4.save(endTime)"
                 ></v-time-picker>
@@ -128,8 +104,8 @@
               </div>
               <v-spacer></v-spacer>
               <v-select
-                class="ml-10"
                 v-model="markerIcon"
+                class="ml-10"
                 :items="markerIcons"
                 menu-props="auto"
                 label="Select"
@@ -137,7 +113,7 @@
                 single-line
               >
                 <template v-slot:prepend>
-                  <img v-model="iconSvg" width="25px" :src="iconSvg">
+                  <img width="25px" :src="iconSvg" />
                 </template>
               </v-select>
             </div>
@@ -145,84 +121,16 @@
         </v-card>
       </v-col>
     </v-row>
-    <v-dialog v-model="dialog" width="400">
-      <v-card ref="form" class="pa-4">
-        <v-card-text>
-          <v-row align="center" no-gutters>
-            <div>
-              Avatar Image
-            </div>
-            <v-spacer></v-spacer>
-            <v-avatar size="100">
-              <v-hover v-slot="{ hover }">
-                <v-img
-                  lazy-src="https://picsum.photos/id/11/10/6"
-                  src="https://picsum.photos/id/11/500/300"
-                >
-                  <v-expand-transition>
-                    <div
-                      v-if="hover"
-                      class="d-flex transition-fast-in-fast-out light-green darken-2 v-card--reveal display-1 white--text"
-                      style="height: 100%; width: 100%; padding-left: 20px; padding-top: 30px"
-                      @click="editImage"
-                    >
-                      Edit
-                    </div>
-                  </v-expand-transition>
-                </v-img>
-              </v-hover>
-            </v-avatar>
-          </v-row>
-          <v-text-field
-            ref="name"
-            v-model="name"
-            :rules="[() => !!name || 'This field is required']"
-            :error-messages="errorMessages"
-            label="Full Name"
-            :placeholder="$store.state.username"
-            required
-          ></v-text-field>
-        </v-card-text>
-        <v-divider class="mt-12"></v-divider>
-        <v-card-actions>
-          <v-btn text @click="openDialog">
-            close
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-slide-x-reverse-transition>
-            <v-tooltip v-if="formHasErrors" left>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  icon
-                  class="my-0"
-                  v-bind="attrs"
-                  @click="resetForm"
-                  v-on="on"
-                >
-                  <v-icon>mdi-refresh</v-icon>
-                </v-btn>
-              </template>
-              <span>Refresh form</span>
-            </v-tooltip>
-          </v-slide-x-reverse-transition>
-          <v-btn color="primary" text @click="submit">
-            Submit
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-container>
 </template>
 
 <script>
-import { api } from '../../apis/api'
 
+import EditNameCard from "../atoms/EditNameCard";
 export default {
   name: '',
+  components: {EditNameCard},
   data: () => ({
-    errorMessages: '',
-    name: null,
-    formHasErrors: false,
     dialog: false,
     position: '',
     startTime: '',
@@ -231,74 +139,24 @@ export default {
     menu2: false,
     menu3: false,
     iconSvg: require('../../assets/markers/food.svg'),
-    markerIcons: [
-      'Food',
-      'CoWorking Space',
-      'Car Rental',
-      'Karaoke'
-    ],
+    markerIcons: ['Food', 'CoWorking Space', 'Car Rental', 'Karaoke'],
     icons: new Map([
-      [ 'Food', 'food.svg'],
-      [ 'CoWorking Space', 'co-working-space.svg'],
-      [ 'Car Rental', 'car.svg'],
-      [ 'Karaoke', 'karaoke.svg']
-    ])
+      ['Food', 'food.svg'],
+      ['CoWorking Space', 'co-working-space.svg'],
+      ['Car Rental', 'car.svg'],
+      ['Karaoke', 'karaoke.svg'],
+    ]),
   }),
 
-  computed: {
-    form() {
-      return {
-        name: this.name,
-      }
-    },
-  },
-
   watch: {
-    name() {
-      this.errorMessages = ''
-    },
     markerIcon(val) {
       this.iconSvg = require('../../assets/markers/' + this.icons.get(val))
-    }
+    },
   },
 
   methods: {
     log() {
       console.log(this.markerIcon)
-    },
-    addressCheck() {
-      this.errorMessages = this.address && !this.name ? `Hey! I'm required` : ''
-
-      return true
-    },
-    resetForm() {
-      this.errorMessages = []
-      this.formHasErrors = false
-
-      Object.keys(this.form).forEach(f => {
-        this.$refs[f].reset()
-      })
-    },
-    submit() {
-      this.formHasErrors = false
-      const body = {
-        userId: this.$store.state.user.username,
-        name: this.name,
-      }
-      Object.keys(this.form).forEach(f => {
-        if (!this.form[f]) this.formHasErrors = true
-
-        this.$refs[f].validate(true)
-
-        if (!this.formHasErrors) {
-          this.$store.commit('setUsername', this.name)
-          api.saveUser(body)
-        }
-      })
-      this.dialog = false
-    },
-    editImage() {
-      console.log('click')
     },
     openDialog() {
       if (this.dialog) {
